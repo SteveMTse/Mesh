b89ac730af9078eed898e3df428de0815762a723
SteveMTse <jwyxxxwb@gmail.com>
2018-08-08 13:10:53 -0400

    modified

--- Mesh.hpp
+++ Mesh.hpp
@@ -11,6 +11,11 @@ struct Point {
     double y;
 };
 
+struct Polygon {
+    int index;
+    vector<int> vertices;
+};
+
 class Mesh {
     private:
         //Domain
@@ -24,6 +29,8 @@ class Mesh {
         int number_of_elements = -1;
         Point* nodes = NULL;
 
+        Polygon* elements = NULL;
+
     public:
         Mesh(double xl, double xr, double yb, double yt, int NNode, int NElt) {
             this -> xleft = xl;
@@ -33,10 +40,27 @@ class Mesh {
             this -> number_of_nodes = NNode;
             this -> number_of_elements = NElt;
             this -> nodes = new Point[NNode];
+            this -> elements = new Polygon[NElt];
+        }
+
+        void set_node(int index, double x, double y) {
+            (this -> nodes[index]).index = index + 1;
+            (this -> nodes[index]).x = x;
+            (this -> nodes[index]).y = y;
+        }
+
+        void init_polygon(int index, int NVertices) {
+            (this -> elements[index]).index = index + 1;
+            (this -> elements[index]).vertices.reserve(NVertices);
+        }
+
+        void set_polygon(int index, int elt) {
+            (this -> elements[index]).vertices.push_back(elt);
         }
 
         ~Mesh() {
             delete[] this -> nodes;
+            delete[] this -> elements;
         }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include "Mesh.hpp"
 
 int main(int argc, char* argv[]) {
+
+    Mesh m = Mesh(-1, 1, -1, 1, 100, 100);
+    m.set_node(0, 1, 1);
+    m.init_polygon(0, 3);
+    m.set_polygon(0, 1);
+    m.set_polygon(0, 2);
+    m.set_polygon(0, 3);
     return 0;
 }
